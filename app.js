c7f86aeeebfae724ccb2ffd8d53c7539577f92a2
Hung Hay Hat <diegocoshung19@gmail.com>
2024-04-01 19:03:01 +0700

    Enhanced object literals

--- app.js
+++ app.js
@@ -16,3 +16,29 @@ const course = {
 }
 
 console.log(course.getName());
+
+// ECMAScript 6 
+function arrToObj (arr){
+    return arr.reduce((key, value) => {
+        key[value[0]] = value[1]
+        return key
+    },{})
+}
+
+// Expected results:
+
+const obj1 = arrToObj([
+    ['name', 'Son Dang'], 
+    ['age', 21], 
+    ['address', 'Ha Noi']
+ ])
+ console.log(obj1)
+ // Output: { name: 'Son Dang', age: 21, address: 'Ha Noi' }
+ 
+ const obj2 = arrToObj([
+    ['name', 'Duc Long'], 
+    ['age', 18], 
+    ['address', 'Ha Noi']
+ ])
+ console.log(obj2)
+ // Output: { name: 'Duc Long', age: 18, address: 'Ha Noi' }
