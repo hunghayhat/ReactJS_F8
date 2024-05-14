9c6d947ec9cab95eb93db66f79ce0ec7cb06ac93
Hung Hay Hat <diegocoshung19@gmail.com>
2024-05-14 22:40:53 +0700

    promise chain

--- promise/app.js
+++ promise/app.js
@@ -1,22 +1,29 @@
 var promise = new Promise(
     // Executor
-    function(resolve, reject) {
+    function (resolve, reject) {
         // Logic
         // Thành công: resolve()
         // Thất bại: reject()
 
         //fake call API
-        reject('Co loi');
+        resolve('123')
     });
 
 promise
-    .then(function() {
-        console.log('Check');
+    .then(function () {
+        return new Promise(function (resolve) {
+            setTimeout(resolve, 3000)
+        })
     })
 
-    .catch(function(error) {
+    .then(function (data) {
+        console.log(data);
+    })
+
+
+    .catch(function (error) {
         console.log(error);
-    })    
-    .finally(function() {
+    })
+    .finally(function () {
         console.log('Done');
     })
